7508551f427b50add0f41f1a4223a940f33eaea6
haha <kevin881127@email.com>
2025-12-15 18:38:57 +0800

    修正 rop/ret2mprotect/vuln.c 的緩衝區溢位漏洞

--- Stack-Based-Buffer-Overflows-main/rop/ret2mprotect/vuln.c
+++ Stack-Based-Buffer-Overflows-main/rop/ret2mprotect/vuln.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 int main(int argc, char **argv){
-
     char buffer[260];
     int len, i;
     printf("Welcome in Birby Enterprise\n");
     printf("To join the company, please register with your full name: ");
-    gets(buffer);
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        // 移除字串結尾的換行符
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
     len = strlen(buffer);
     printf("Welcome %s in Birby Enterprise\n", buffer);
-
     return 0;
-
 }
